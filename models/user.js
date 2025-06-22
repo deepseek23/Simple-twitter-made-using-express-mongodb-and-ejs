5201b57e7aa9d5ed9ff44ac426a55e16bb8fc49e
tarun <t.s.clashers@gmail.comn>
2025-06-23 02:22:35 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://127.0.0.1:27017/miniproject`);
+
+const userSchema = mongoose.Schema({
+    name: String,
+    username: String,
+    age: Number,
+    email: String,
+    password: String,
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"post"
+        }
+    ],
+    profileepic:{
+        type: String,
+        default: "default.jpg"
+    }
+})
+
+module.exports = mongoose.model("user", userSchema);
