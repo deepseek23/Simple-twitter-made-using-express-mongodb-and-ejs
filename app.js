4b198b275ea6274e20d1691e8410fa510b4a83b8
Tarun Munjani <t.s.clashers@gmail.com>
2025-06-23 02:24:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -155,26 +155,3 @@ app.listen(9000);
 
 
 
-//  multer
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "./public/images/uploads");
-//   },
-//   filename: function (req, file, cb) {
-//     crypto.randomBytes(12, (err, bytes) => {
-//       const fn = bytes.toString("hex") + path.extname(file.originalname);
-//       cb(null, fn);
-//     });
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
-// app.get("/test", (req, res) => {
-//   res.render("test");
-// });
-
-// app.post("/upload", upload.single("image"), (req, res) => {
-//   console.log(req.file);
-//   res.redirect("/test");
-// });
